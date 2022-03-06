a865c4920585b3482f645283a3e559e7e84171e7
sabinlike <sabinarustamova482@gmail.com>
2022-03-06 14:14:49 +0500

    Initial commit

---
+++ home2.py
@@ -0,0 +1,9 @@
+my_int = 7
+my_float = 2.5
+my_str = "Hello student"
+my_list = ['r', '2']
+my_tuple = ('s', '3')
+my_dict = {'country': 'France', 'city': 'Paris'}
+main_list = [my_int, my_float, my_str, my_list, my_tuple, my_dict]
+for i in main_list:
+    print(f'{i} is {type(i)}')
